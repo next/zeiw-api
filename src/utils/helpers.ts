4f8a622718dc1af3ed99081ad4870508ca150392
Hampus Kraft <pnfcre@gmail.com>
2020-05-20 21:17:00 +0200

    :bug: Fix a minor bug

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -4,7 +4,7 @@ export function getFlags(flags: number, flagsObject: keyValue) {
 	const outFlags: string[] = []
 
 	Object.entries(flagsObject).forEach(([key, value]) => {
-		if (flags & value) outFlags.push(key)
+		if (Number(flags) & value) outFlags.push(key)
 	})
 
 	return outFlags
